72366db4041ee8c6496de77717eacbda339dc2ee
jamiro <jamiro@play-debian.voidcolours.org>
2025-04-06 11:22:21 +0200

    added command to be used as snippet

--- nldates.ts
+++ nldates.ts
@@ -1,5 +1,6 @@
-import { readSettings, writeSettings } from "$sb/lib/settings_page.ts";
-import * as chrono from 'chrono-node';
+import { readSetting, writeSetting } from "jsr:@silverbulletmd/silverbullet@^0.10.4//plug-api/lib/settings_page.ts";
+
+import * as chrono from 'npm:chrono-node';
 
 export async function parseDate(){
   // get prefix from settings
@@ -15,6 +16,25 @@ export async function parseDate(){
 
   let txt = await syscall("editor.prompt", "what is the date?")
   let res = chrono.parseDate(txt)
+  let date = new Date(res).toISOString().split('T')[0]
+  // actually write to text
+  let finstring = "[[" + prefix + date + "]]"
+  await syscall("editor.insertAtCursor", finstring)
+}
+
+export async function parseDatecmd(text){
+  // get prefix from settings
+  let presettingsobj = await readSettings({nldates: {}})
+  let settingsobj = presettingsobj["nldates"]
+  if (Object.keys(settingsobj).length == 0) { // no settings obj
+    await syscall("editor.flashNotification", "Change prefix in SETTINGS")
+    let newobj = {nldates: {"dailyPrefix":""}}
+    await writeSettings(newobj)
+    settingsobj["dailyPrefix"] = ""
+  }
+  let prefix = settingsobj["dailyPrefix"]
+
+  let res = chrono.parseDate(text)
   let date = new Date(res).toISOString().split('T')[0]
   // actually write to text
   let finstring = "[[" + prefix + date + "]]"
